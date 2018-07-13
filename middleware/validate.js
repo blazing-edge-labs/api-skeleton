5aae221da975d6cf1cb1fbf0d4d3898d6a36af00
Robert Katić <robert@blazingedge.io>
2018-07-13 15:45:59 +0200

    Make validation definitions cleaner.

--- middleware/validate.js
+++ middleware/validate.js
@@ -9,30 +9,30 @@ const defaults = {
   convert: true,
 }
 
-const path = {
+const paths = {
   body: 'request.body',
   header: 'request.header',
   param: 'params',
   query: 'request.query',
 }
 
-function validate (target, schema, options = {}) {
+const validator = (path, target) => (schema, options = {}) => {
   const opts = _.defaults(options, defaults)
   const schemaCompiled = joi.compile(schema)
 
   return async function (ctx, next) {
-    const input = _.get(ctx, path[target])
-    ctx.assert(input, 400)
+    const input = _.get(ctx, path)
 
     const {error: err, value: data} = schemaCompiled.validate(input, opts)
     if (err) {
       err.target = target
-      throw error.validation('http.bad_request', 400)(err)
+      throw new error.ValidationError('http.bad_request', err, 400)
     }
 
-    _.set(ctx, `v.${target}`, _.assign({}, _.get(ctx, `v.${target}`), data))
+    _.update(ctx, `v.${target}`, prevData => ({...prevData, ...data}))
+
     await next()
   }
 }
 
-module.exports = validate
+module.exports = _.mapValues(paths, validator)
--- route/superadmin.js
+++ route/superadmin.js
@@ -14,52 +14,67 @@ router.use(auth)
 router.use(roleUser.gte(konst.roleUser.superadmin))
 
 resources.forEach(resource => {
-  router.get(`/${resource}/many`, validate('query', {
-    ids: joi.array().items(joi.number().integer()),
-  }), async (ctx) => {
-    const {ids} = ctx.v.query
-    ctx.state.r = await adminRepo[resource].getMany(ids)
-  })
-
-  router.get(`/${resource}`, validate('query', {
-    sort: joi.array().items(joi.string()).length(2),
-    page: joi.number(),
-    perPage: joi.number(),
-    filter: joi.object(),
-  }), async function (ctx) {
-    const {query} = ctx.v
-    const count = await adminRepo[resource].getAllCount()
-    ctx.state.r = {
-      items: await adminRepo[resource].getAll(query),
-      count: count[0].total,
+  router.get(`/${resource}/many`,
+    validate.query({
+      ids: joi.array().items(joi.number().integer()),
+    }),
+    async (ctx) => {
+      const {ids} = ctx.v.query
+      ctx.state.r = await adminRepo[resource].getMany(ids)
     }
-  })
+  )
 
-  router.get(`/${resource}/:id`, validate('param', {
-    id: joi.number().integer().positive().required(),
-  }), async function (ctx) {
-    const {id} = ctx.v.param
-    ctx.state.r = await adminRepo[resource].getById(id)
-  })
+  router.get(`/${resource}`,
+    validate.query({
+      sort: joi.array().items(joi.string()).length(2),
+      page: joi.number(),
+      perPage: joi.number(),
+      filter: joi.object(),
+    }),
+    async (ctx) => {
+      const {query} = ctx.v
+      const count = await adminRepo[resource].getAllCount()
+      ctx.state.r = {
+        items: await adminRepo[resource].getAll(query),
+        count: count[0].total,
+      }
+    }
+  )
 
-  router.put(`/${resource}/:id`, validate('param', {
-    id: joi.number().integer().positive().required(),
-  }), async function (ctx) {
-    const {id} = ctx.v.param
-    ctx.state.r = await adminRepo[resource].update(id, ctx.request.body)
-  })
+  router.get(`/${resource}/:id`,
+    validate.param({
+      id: joi.number().integer().positive().required(),
+    }),
+    async function (ctx) {
+      const {id} = ctx.v.param
+      ctx.state.r = await adminRepo[resource].getById(id)
+    }
+  )
+
+  router.put(`/${resource}/:id`,
+    validate.param({
+      id: joi.number().integer().positive().required(),
+    }),
+    async function (ctx) {
+      const {id} = ctx.v.param
+      ctx.state.r = await adminRepo[resource].update(id, ctx.request.body)
+    }
+  )
 
   router.post(`/${resource}`, async (ctx) => {
     ctx.state.r = await adminRepo[resource].create(ctx.request.body)
   })
 
-  router.del(`/${resource}/:id`, validate('param', {
-    id: joi.number().integer().positive().required(),
-  }), async function (ctx) {
-    const {id} = ctx.v.param
-    await adminRepo[resource].remove(id)
-    ctx.state.r = {}
-  })
+  router.del(`/${resource}/:id`,
+    validate.param({
+      id: joi.number().integer().positive().required(),
+    }),
+    async function (ctx) {
+      const {id} = ctx.v.param
+      await adminRepo[resource].remove(id)
+      ctx.state.r = {}
+    }
+  )
 })
 
 module.exports = router
