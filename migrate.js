1c25ebc7f830171674b66916fe39906b2a478116
Robert Katić <robert@blazingedge.io>
2018-12-22 20:42:03 +0100

    Refactor migrate.js

--- migrate.js
+++ migrate.js
@@ -7,44 +7,36 @@ const migratio = require('migratio')
 const { db, sql, pgp } = require('db')
 
 const command = argv._[0]
-const opts = {
-  directory: 'migration',
-  tableName: 'migration',
-  verbose: true,
-}
 
-function end () {
-  process.exit(0)
-}
-
-function finalize () {
-  pgp.end()
-}
-
-function fail (err) {
-  console.error(err)
-  process.exit(1)
-}
-
-if (argv.r) {
-  opts.revision = argv.r
-}
-
-(function () {
+async function run () {
   switch (command) {
     case 'up':
-      return migratio.up(opts).finally(finalize).then(end, fail)
     case 'down':
-      return migratio.down(opts).finally(finalize).then(end, fail)
     case 'current':
-      return migratio.current(opts).finally(finalize).then(end, fail)
+      return migratio[command]({
+        directory: 'migration',
+        tableName: 'migration',
+        verbose: true,
+        revision: argv.r,
+      })
     case 'recreate':
-      return db.query(sql('schema')).finally(finalize).then(end, fail)
+      return db.query(sql('schema'))
     case 'seed':
-      return db.tx(function (t) {
+      return db.tx(t => {
         return t.query(sql('seed'))
-      }).finally(finalize).then(end, fail)
+      })
     default:
       throw new Error(`"${command}" is not a valid migration command`)
   }
-})()
+}
+
+run()
+.then(() => {
+  pgp.end()
+  return process.exit(0)
+})
+.catch(e => {
+  console.error(e)
+  pgp.end()
+  return process.exit(1)
+})
