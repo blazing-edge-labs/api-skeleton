5e857035087b68cf8a8c4bb86ec3657c9464682d
Robert Katić <robert@blazingedge.io>
2019-03-02 09:53:41 +0100

    Implement maploading

--- repo/base.js
+++ repo/base.js
@@ -1,16 +1,25 @@
 const _ = require('lodash')
 
+const { db, pgp } = require('db')
+const error = require('error')
+
+const { format, csv } = pgp.as
+
+const CHUNK_SIZE = 250
+
 function mapper (mapping) {
-  const entries = Object.entries(mapping).map(([name, fx]) => [name, fx, _.isFunction(fx)])
+  const [ extras, nonExtras ] = _.partition(Object.entries(mapping), pair => _.isArray(pair[1]))
+
+  const ownMapping = nonExtras.map(([name, fx]) => [name, fx, _.isFunction(fx)])
 
   function mapItem (item) {
     const res = {}
 
     // optimized for performance (this code is potentially run on large datasets)
-    for (const [ name, fx, isFun ] of entries) {
+    for (const [ name, fx, isFun ] of ownMapping) {
       const value = isFun ? fx(item) : item[fx]
 
-      // ignore undefined values, useful if used to prepare data for DB
+      // ignore undefined values
       if (value !== void 0) {
         res[name] = value
       }
@@ -19,15 +28,89 @@ function mapper (mapping) {
     return res
   }
 
-  const map = data => {
+  function map (data) {
     if (!data) return null
     return _.isArray(data) ? data.map(mapItem) : mapItem(data)
+  }
+
+  map.loading = (includes, opts) => async data => {
+    if (!data) return null
+
+    const raw = _.castArray(data)
+    const mapped = raw.map(mapItem)
+    const getColumn = _.memoize(col => raw.map(r => r[col]))
+
+    await Promise.all(extras.map(extra => {
+      const [name, [keyCol, resolver]] = extra
+      if (includes[name]) {
+        return autoChunk(CHUNK_SIZE, resolver(opts, includes[name]), getColumn(keyCol))
+        .then(values => values.forEach((val, i) => {
+          mapped[i][name] = val
+        }))
+      }
+    }))
+
+    return _.isArray(data) ? mapped : mapped[0]
   }
 
   map.mapping = { ...mapping }
   return map
 }
 
+function createResolver (table, keyColumn, { map = _.identity, multi = false, condition = '' } = {}) {
+  const leftPart = format('SELECT * FROM $1~ WHERE $2~ IN', [table, keyColumn])
+  const rightPart = condition ? `AND ${condition}` : ''
+
+  return (opts = {}, includes) => async keys => {
+    if (keys.length === 0) return []
+
+    const t = opts.t || db
+    const rows = await t.any(`${leftPart} (${csv(keys)}) ${rightPart}`).catch(error.db)
+
+    const mapped = _.isEmpty(includes)
+      ? map(rows)
+      : await map.loading(includes, opts)(rows)
+
+    const rowKeys = rows.map(row => row[keyColumn])
+
+    return keys.map(byZipped(rowKeys, mapped, multi))
+  }
+}
+
+async function autoChunk (chunkSize, resolve, keys) {
+  if (keys.length <= chunkSize) {
+    return resolve(keys)
+  }
+
+  const uniqueKeys = [...new Set(keys)]
+
+  const keyChunks = _.chunk(uniqueKeys, chunkSize)
+  const valChunks = await Promise.all(keyChunks.map(resolve))
+
+  return keys.map(byZipped(uniqueKeys, _.flatten(valChunks)))
+}
+
+function byZipped (keys, values, multi = false) {
+  const map = new Map()
+
+  if (multi) {
+    const getArray = key => map.get(key) || map.set(key, []).get(key)
+
+    keys.forEach((key, i) => {
+      getArray(key).push(values[i])
+    })
+
+    return getArray
+  }
+
+  keys.forEach((key, i) => {
+    map.set(key, values[i])
+  })
+
+  return key => map.get(key)
+}
+
 module.exports = {
   mapper,
+  createResolver,
 }
---
+++ repo/base.test.js
@@ -0,0 +1,158 @@
+// const _ = require('lodash')
+
+const test = require('test')
+const { db, helper } = require('db')
+const { mapper: _mapper, createResolver } = require('repo/base')
+
+function callCounter (fun) {
+  const f = function () {
+    f.callCount++
+    return fun.apply(this, arguments)
+  }
+  f.callCount = 0
+  return f
+}
+
+const mapper = (...args) => {
+  const map = _mapper(...args)
+  map.loading = callCounter(map.loading)
+  return map
+}
+
+test.only('map.loading', async t => {
+  // A -> (B+, C+ -> B)
+
+  await db.query(`
+    CREATE TABLE "test_A" (
+      id serial,
+      label text,
+      primary key (id)
+    );
+    INSERT INTO "test_A" (label) VALUES ('A1'),('A2'),('A3');
+
+    CREATE TABLE "test_B" (
+      id serial,
+      a_id int references "test_A" (id),
+      label text,
+      primary key (id)
+    );
+    INSERT INTO "test_B" (label, a_id) VALUES ('B1', 1),('B2', 1),('B3', 2);
+
+    CREATE TABLE "test_C" (
+      id serial,
+      a_id int references "test_A" (id),
+      b_id int references "test_B" (id),
+      label text,
+      primary key (id)
+    );
+    INSERT INTO "test_C" (label, a_id, b_id) VALUES
+    ('C1', 1, 1),
+    ('C2', 1, 2),
+    ('C3', 2, 3);
+  `)
+
+  t.ok(true, 'initiated tables')
+
+  const mapB = mapper({
+    label: 'label',
+  })
+  const bResolver = createResolver('test_B', 'id', { map: mapB })
+  const bOfAResolver = createResolver('test_B', 'a_id', { map: mapB, multi: true })
+
+  const mapC = mapper({
+    label: 'label',
+    b: ['b_id', bResolver],
+  })
+  const cOfAResolver = createResolver('test_C', 'a_id', { map: mapC, multi: true })
+
+  const mapA = mapper({
+    label: 'label',
+    b: ['id', bOfAResolver],
+    c: ['id', cOfAResolver],
+  })
+
+  t.ok(true, 'initiated repo')
+
+  const r = await db.any('SELECT * FROM "test_A"')
+  .then(mapA.loading({
+    b: {},
+    c: {
+      b: {},
+    },
+  }))
+
+  t.is(mapA.loading.callCount, 1)
+  t.is(mapB.loading.callCount, 0)
+  t.is(mapC.loading.callCount, 1)
+
+  t.deepEqual(r, [
+    {
+      'label': 'A1',
+      'b': [
+        {
+          'label': 'B1',
+        },
+        {
+          'label': 'B2',
+        },
+      ],
+      'c': [
+        {
+          'label': 'C1',
+          'b': {
+            'label': 'B1',
+          },
+        },
+        {
+          'label': 'C2',
+          'b': {
+            'label': 'B2',
+          },
+        },
+      ],
+    },
+    {
+      'label': 'A2',
+      'b': [
+        {
+          'label': 'B3',
+        },
+      ],
+      'c': [
+        {
+          'label': 'C3',
+          'b': {
+            'label': 'B3',
+          },
+        },
+      ],
+    },
+    {
+      'label': 'A3',
+      'b': [],
+      'c': [],
+    },
+  ])
+
+  {
+    const data = Array(400 - 3).fill({ label: 'AA' })
+    const columnSet = new helper.ColumnSet(['label'])
+    await db.query(helper.insert(data, columnSet, 'test_A'))
+
+    mapC.loading.callCount = 0
+
+    const a = await db.any('SELECT * FROM "test_A"')
+    .then(mapA.loading({ c: { b: {} } }))
+
+    t.is(a.length, 400)
+    t.is(mapC.loading.callCount, 2, 'auto chunk')
+    t.is(a.map(r => r.c.length).filter(Boolean).length, 2)
+    t.notOk(a[0].b, 'a.b not loaded')
+  }
+
+  await db.query(`
+    DROP TABLE "test_C";
+    DROP TABLE "test_B";
+    DROP TABLE "test_A";
+  `)
+})
