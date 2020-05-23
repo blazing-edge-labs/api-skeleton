79fe22e9e07d17326a222f44f45c1d80219bd93b
Petar Ivancevic <petar.ivancevic171@gmail.com>
2020-05-23 23:06:06 +0200

    updated error validation middleware

--- middleware/errorValidation.js
+++ middleware/errorValidation.js
@@ -1,14 +1,31 @@
+const _ = require('lodash')
+
+const error = require('error')
+
 function errorValidation (errorConstants = []) {
   return async function _errorValidation (ctx, next) {
     if (!next) {
-      return ctx.errorConstants = errorConstants
+      ctx.errorConstantObjs = errorConstants
+      return
     }
 
     try {
       await next()
     } catch (err) {
-      console.error(err)
-      throw err
+      if (err instanceof error.ValidationError || _.includes([401, 500], err.status)) {
+        throw err
+      }
+
+      const isErrorDefinedInConstants = _.some(
+        errorConstants,
+        errorConstant => errorConstant.error === err.error,
+      )
+      if (isErrorDefinedInConstants) {
+        throw err
+      }
+
+      // TODO maybe this should be an internal error or validation one?
+      throw error('http.internal', err)
     }
   }
 }
